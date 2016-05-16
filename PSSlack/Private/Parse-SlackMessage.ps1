cd3dbd891199804224f47b8de7dc0f414ae8860a
RamblingCookieMonster <RamblingCookieMonster@users.noreply.github.com>
2016-05-16 19:56:04 -0400

    Include more properties in parsed messages

--- PSSlack/Private/Parse-SlackMessage.ps1
+++ PSSlack/Private/Parse-SlackMessage.ps1
@@ -16,6 +16,9 @@ Function Parse-SlackMessage {
             Channel = $Message.channel.name
             Text = $Message.text
             Attachments = $Message.Attachments
+            File = $Message.File
+            Type = $Message.Type
+            SubType = $Message.subtype
             Timestamp = ConvertFrom-UnixTime $Message.ts
             Permalink = $Message.permalink
             Previous = Extract-Previous $Message.Previous
