c16b4bf67b3eecc628d16a3924acb94c7c8ecdcb
RamblingCookieMonster <RamblingCookieMonster@users.noreply.github.com>
2016-05-17 09:45:24 -0400

    force....

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 ﻿# Grab nuget bits, install modules, start build.
 Get-PackageProvider -Name NuGet -ForceBootstrap
 
-Install-Module Psake, PSDeploy
+Install-Module Psake, PSDeploy -force
 
 Import-Module Psake
 Invoke-psake .\psake.ps1
