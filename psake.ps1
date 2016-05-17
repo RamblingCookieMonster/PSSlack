88bd9c6bc7be12fbe0212ea5b429f747c25a9e2b
RamblingCookieMonster <RamblingCookieMonster@users.noreply.github.com>
2016-05-17 01:05:06 -0400

    add initial ci/cd scaffolding
    ugly

---
+++ psake.ps1
@@ -0,0 +1,55 @@
+﻿# More to do. Ugly example. I don't like depending on AppVeyor specific env vars...
+
+Properties {
+    [string]$ProjectRoot = $PSScriptRoot #$ENV:APPVEYOR_BUILD_FOLDER # Change based on build solution
+    $Timestamp = Get-date -uformat "%Y%m%d-%H%M%S"
+    $PSVersion = $PSVersionTable.PSVersion.Major
+    $TestFile = "TestResults_PS$PSVersion`_$TimeStamp.xml"
+
+    $Address = "https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)"
+
+    $Verbose = @{}
+    if($env:APPVEYOR_REPO_BRANCH -and $env:APPVEYOR_REPO_BRANCH -notlike "master")
+    {
+        $Verbose.add("Verbose",$True)
+    }
+}
+
+Task Default -Depends Deploy
+
+Task Init {
+    Set-Location $ProjectRoot
+}
+
+Task Clean {
+    Remove-Item "$PSScriptRoot\Destination\" -ErrorAction SilentlyContinue -Force -Recurse
+}
+
+Task Test -Depends Clean {
+    $TestResults = Invoke-Pester @verbose -Path $ProjectRoot\Tests -PassThru -OutputFormat NUnitXml -OutputFile "$ProjectRoot\$TestFile"
+
+    if($TestResults.FailedCount -gt 0)
+    {
+        Write-Error "Failed '$($TestResults.FailedCount)' tests, build failed"
+    }
+
+    #Run a test with the current version of PowerShell, upload results    
+    "`n`tSTATUS: Testing with PowerShell $PSVersion"
+
+    If($env:APPVEYOR_JOB_ID)
+    {
+        (New-Object 'System.Net.WebClient').UploadFile( $Address, "$ProjectRoot\$TestFile" )
+        Remove-Item "$ProjectRoot\$TestFile" -Force -ErrorAction SilentlyContinue
+    }
+
+    Remove-Item "$ProjectRoot\$TestFile" -Force -ErrorAction SilentlyContinue
+
+    if($TestResults.FailedCount -gt 0)
+    {
+        Write-Error "Failed '$($TestResults.FailedCount)' tests, build failed"
+    }
+}
+
+Task Deploy -Depends Test {
+    "Deploying"
+}
