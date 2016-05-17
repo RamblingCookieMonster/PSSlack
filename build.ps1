f79f31b968005dc14bced0510328b2247dd5fff6
RamblingCookieMonster <RamblingCookieMonster@users.noreply.github.com>
2016-05-17 09:39:42 -0400

    fix nuget dependency

--- build.ps1
+++ build.ps1
@@ -1,3 +1,7 @@
-﻿Install-Module Psake
+﻿# Grab nuget bits, install modules, start build.
+Get-PackageProvider -Name NuGet -ForceBootstrap
+
+Install-Module Psake, PSDeploy
+
 Import-Module Psake
 Invoke-psake .\psake.ps1
