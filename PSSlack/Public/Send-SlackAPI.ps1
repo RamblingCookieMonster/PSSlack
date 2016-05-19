6a75b74ed8883f6e96d3f9efbe2dfc47e94c73dc
Adam Bertram <adbertram@gmail.com>
2016-05-19 14:01:56 -0700

    Revert "Revert "ignored per environment configuration xml""
    This reverts commit 394b17b6c09a26ae653010fbf85c5058a805f6e7.

--- PSSlack/Public/Send-SlackAPI.ps1
+++ PSSlack/Public/Send-SlackAPI.ps1
@@ -25,15 +25,29 @@
     [OutputType([String])]
     [cmdletbinding()]
     param (
-        $Method,
-        $Body = @{},
-        $Token = $Script:PSSlack.Token
+        [Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[string]$Method,
+		
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[hashtable]$Body = @{ },
+	
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[ValidateScript({
+			if (-not $Script:PSSlack.Token)
+			{
+				throw 'Please supply a Slack Api Token with Set-SlackApiToken.'
+			}
+			else
+			{
+				$true	
+			}
+		})]
+		[string]$Token = $Script:PSSlack.Token
     )
 
-    if ([string]::isnullorempty($token)){
-        throw "Please supply a Slack Api Token with Set-SlackApiToken."
-    }
-
     $Body.token = $Token
-    Invoke-RestMethod -Uri "https://slack.com/api/$method" -body $Body
+    Invoke-RestMethod -Uri "https://slack.com/api/$Method" -body $Body
 }
