00924ca8e6b4d2892df9432d07062c046f9b8360
Matt Cummins <kittiah@gmail.com>
2016-08-17 10:32:27 +0100

    Adding support for Slack Private Groups

---
+++ PSSlack/Public/Get-SlackGroup.ps1
@@ -0,0 +1,102 @@
+﻿function Get-SlackGroup {
+    <#
+    .SYNOPSIS
+        Get information about Slack groups
+
+    .DESCRIPTION
+        Get information about Slack groups
+
+    .PARAMETER Token
+        Specify a token for authorization.
+
+        See 'Authentication' section here for more information: https://api.slack.com/web
+        Test tokens are a simple way to use this
+
+    .PARAMETER Name
+        One or more group names to return.  Defaults to all.  Accepts wildcards.
+
+    .PARAMETER ExcludeArchived
+        Whether to exclude archived groups. Default is to include all.
+
+    .PARAMETER Raw
+        If specified, we provide raw output and do not parse any responses
+
+    .FUNCTIONALITY
+        Slack
+    #>
+    [cmdletbinding()]
+    param (
+        $Token = $Script:PSSlack.Token,
+        [string[]]$Name,
+        [switch]$ExcludeArchived,
+        [switch]$Raw
+    )
+    end
+    {
+        Write-Verbose "$($PSBoundParameters | Out-String)"
+
+        if($ExcludeArchived)
+        {
+            $body = @{ exclude_archived = 1 }
+        }
+        else
+        {
+            $body = @{ exclude_archived = 0 }
+        }
+        $params = @{
+            Body = $body
+            Token = $Token
+            Method = 'groups.list'
+        }
+        $RawGroups = Send-SlackApi @params
+
+        $HasWildCard = $False
+        foreach($Item in $Name)
+        {
+            if($Item -match '\*')
+            {
+                $HasWildCard = $true
+                break
+            }
+        }
+
+        if($Name -and -not $HasWildCard)
+        {       
+            # torn between independent queries, or filtering groups.list
+            # submit a PR if this isn't performant enough or doesn't make sense.
+            $Groups = $RawGroups.groups |
+                Where {$Name -Contains $_.name}
+        }
+        elseif ($Name -and$HasWildCard)
+        {
+            $AllGroups = $RawGroups.groups
+            
+            # allow like operator on each group requested in the param, avoid dupes
+            $GroupHash = [ordered]@{}
+            foreach($SlackGroup in $AllGroups)
+            {
+                foreach($Chan in $Name)
+                {
+                    if($SlackGroup.Name -like $Chan -and -not $GroupHash.Contains($SlackGroup.id))
+                    {
+                        $GroupHash.Add($SlackGroup.Id, $SlackGroup)
+                    }
+                }
+            }
+            $Groups = $GroupHash.Values
+        }
+        else # nothing specified
+        {
+            $Groups = $RawGroups.groups
+        }
+
+        if($Raw)
+        {
+            $RawGroups
+        }
+        else
+        {
+            Parse-SlackGroup -InputObject $Groups
+        }
+    }
+}
---
+++ PSSlack/Public/Get-SlackGroupHistory.ps1
@@ -0,0 +1,211 @@
+﻿function Get-SlackGroupHistory {
+    <#
+    .SYNOPSIS
+        Get history from a Slack group
+
+    .DESCRIPTION
+        Get history from a Slack group
+
+    .PARAMETER Token
+        Specify a token for authorization.
+
+        See 'Authentication' section here for more information: https://api.slack.com/web
+        Test tokens are a simple way to use this
+
+    .PARAMETER Id
+        One or more group IDs to extract history from.
+
+    .PARAMETER Before
+        Return history from before this date
+
+    .PARAMETER After
+        Return history from after this date
+
+    .PARAMETER Inclusive
+        If specified, include history from the date specified in Before and/or After parameters
+
+    .PARAMETER Count
+        Number of messages to return per query.  Defaults to 100.  Max 1000
+
+    .PARAMETER Paging
+        If specified, and more data is available with a given 'Count', continue querying Slack until
+            we have retrieved all the data available.
+
+        WARNING: This parameter is experimental
+
+    .PARAMETER MaxQueries
+        Limit the count of API queries to this number.  Only used if you enable -Paging
+
+    .PARAMETER Raw
+        If specified, we provide raw output and do not parse any responses
+
+    .FUNCTIONALITY
+        Slack
+    #>
+    [cmdletbinding()]
+    param (
+        $Token = $Script:PSSlack.Token,
+        [parameter( ValueFromPipelineByPropertyName = $True)] # a bit iffy... ID is common...
+        [Alias('ID')]
+        [string[]]$GroupID,
+
+        [ValidateRange(1,1000)]
+        [int]$Count = 100,
+        [switch]$Inclusive,
+        [datetime]$Before,
+        [datetime]$After,
+        [switch]$Paging,
+        [int]$MaxQueries,
+        [switch]$Raw
+
+    )
+    begin
+    {
+        function Get-UnixTime {
+            param($Date)
+            $unixEpochStart = new-object DateTime 1970,1,1,0,0,0,([DateTimeKind]::Utc)
+            [int]($Date.ToUniversalTime() - $unixEpochStart).TotalSeconds
+        }
+
+        Write-Verbose "$($PSBoundParameters | Out-String)"
+
+        $body = @{
+            channel = $null
+            count = $count
+        }
+        if($Paging)
+        {
+            $PageDirection = 'Backward'
+        }
+        $BeforeTS = $null
+        $AfterTS = $null
+        if($PSBoundParameters.ContainsKey('Before'))
+        {
+            $BeforeTS = Get-UnixTime -Date $Before
+            $body.add('latest', $BeforeTS)
+        }
+        if($PSBoundParameters.ContainsKey('After'))
+        {
+            $AfterTS = Get-UnixTime -Date $After
+            $body.add('oldest', $AfterTS)
+            if(-not $PSBoundParameters.ContainsKey('Before') -and $Paging)
+            {
+                $PageDirection = 'Forward'
+            }
+        }
+        if($Inclusive)
+        {
+            $body.add('inclusive', 1)
+        }
+        $params = @{
+            Token = $Token
+            Method = 'groups.history'
+            Body = $body
+        }
+        $Queries = 1
+
+    }
+    process
+    {
+        foreach($ID in $GroupID)
+        {
+            $has_more = $false
+            $Params.body.channel = $ID
+            do
+            {
+                if($has_more)
+                {
+                    if($Params.Body.oldest)
+                    {
+                        [void]$Params.Body.remove('oldest')
+                    }
+                    if($Params.Body.latest)
+                    {
+                        [void]$Params.Body.remove('latest')
+                    }
+                    if($PageDirection -eq 'Forward')
+                    {
+
+                        $ts = $response.messages.ts | sort | Select -last 1
+                        $Params.body.oldest = $ts
+                        Write-Debug "Paging Forward.`n$(
+                            [pscustomobject]@{
+                                After = $After
+                                Before = $Before
+                                LastTS = $response.messages[-1].ts
+                                SortLast = $response.messages.ts | sort | Select -last 1
+                                SortFirst = $response.messages.ts | sort | Select -first 1
+                                ts = $ts
+                            } | Out-String
+                        )"
+                    }
+                    elseif($PageDirection -eq 'Backward')
+                    {
+                        $ts = $response.messages[-1].ts
+                        if($AfterTS -and $ts -lt $AfterTS)
+                        {
+                            Write-Debug "TS is less than AfterTS, breaking!"
+                            break
+                        }
+                        $Params.body.latest = $ts
+                        Write-Debug "Paging Forward.`n$(
+                            [pscustomobject]@{
+                                After = $After
+                                Before = $Before
+                                LastTS = $response.messages[-1].ts
+                                SortLast = $response.messages.ts | sort | Select -last 1
+                                SortFirst = $response.messages.ts | sort | Select -first 1
+                                ts = $ts
+                            } | Out-String
+                        )"
+                    }
+
+                    $has_more = $false
+                    Write-Debug "Body is now:$($params.body | out-string)"
+                }
+                $response = Send-SlackApi @params
+
+                Write-Debug "$($Response | Format-List -Property * | Out-String)"
+
+                if ($response.ok)
+                {
+
+                    if($response.psobject.properties.name -contains 'has_more' -and $response.has_more)
+                    {
+                        Write-Debug 'Paging engaged!'
+                        $has_more = $true
+                    }
+
+                    if($Raw)
+                    {
+                        $link = "$($Script:PSSlack.ArchiveUri)/$($response.group)/p$($response.ts -replace '\.')"
+                        $response | Add-Member -MemberType NoteProperty -Name link -Value $link
+                        $response
+                    }
+                    else
+                    {
+                        #Order our messages appropriately according to page direction
+                        if($Paging -and $PageDirection -eq 'Forward')
+                        {
+                            Parse-SlackMessage -InputObject $Response | Sort TimeStamp
+                        }
+                        else
+                        {
+                            Parse-SlackMessage -InputObject $Response
+                        }
+                    }
+                }
+                else 
+                {
+                    $response
+                }
+                $Queries++
+            }
+            until (
+                -not $Paging -or
+                -not $has_more -or
+                ($MaxQueries -and $Queries -gt $MaxQueries)
+            )
+        }
+    }
+}
