e9d0ef02bb731f29b23e531afd7da982ec353bf6
Kanji Bates <github.com@kanjibates.com>
2017-10-14 02:42:43 -0400

    Stripped trailing whitespace

--- PSSlack/Private/Parse-SlackTeam.ps1
+++ PSSlack/Private/Parse-SlackTeam.ps1
@@ -14,7 +14,7 @@ Function Parse-SlackTeam {
             Icon = [Ordered] @{
                 Image34 = $Team.icon.image_34
                 Image44 = $Team.icon.image_44
-                Image68 = $Team.icon.image_68                    
+                Image68 = $Team.icon.image_68
                 Image88 = $Team.icon.image_88
                 Image102 = $Team.icon.image_102
                 Image132 = $Team.icon.image_132
