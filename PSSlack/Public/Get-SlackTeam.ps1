8ab141ef115183989a7c1d070df6a9aec3a8e47f
Kanji Bates <github.com@kanjibates.com>
2017-10-14 00:46:11 -0400

    Initial pass at Get-SlackTeam (team.info)

---
+++ PSSlack/Public/Get-SlackTeam.ps1
@@ -0,0 +1,50 @@
+﻿function Get-SlackTeam {
+    <#
+    .SYNOPSIS
+        Get info about the current Slack team.
+
+    .DESCRIPTION
+        Get info about the current Slack team.
+
+    .PARAMETER Token
+        Token to use for the Slack API.
+
+        Default value is the value set by Set-PSSlackConfig.
+
+    .PARAMETER Raw
+        Return raw output.
+
+    .EXAMPLE
+        Get-SlackTeam
+
+    .FUNCTIONALITY
+        Slack
+
+    .LINK
+        https://api.slack.com/methods/team.info
+    #>
+
+    [CmdletBinding()]
+    param (
+        [string]$Token = $Script:PSSlack.Token,
+        [switch]$Raw
+    )
+    end
+    {
+        $params = @{
+            Token = $Token
+            Method = 'team.info'
+        }
+
+        $RawTeam = Send-SlackApi @params
+
+        if($Raw)
+        {
+            $RawTeam
+        }
+        else
+        {
+            $RawTeam
+        }
+    }
+}
