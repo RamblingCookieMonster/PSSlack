b1cf70274252d966320dd8c3a2f4a1f7436ce13c
Kanji Bates <github.com@kanjibates.com>
2017-10-31 00:14:39 -0400

    Add Get-SlackAuth, Parse-SlackAuth, and Test-SlackAuth
    Closes RamblingCookieMonster/PSSlack#2

---
+++ PSSlack/Private/Parse-SlackAuth.ps1
@@ -0,0 +1,25 @@
+﻿# Parse auth.test
+Function Parse-SlackAuth {
+    [CmdletBinding()]
+    param( $InputObject )
+
+    foreach($Auth in $InputObject)
+    {
+        [PSCustomObject] @{
+            PSTypeName = 'PSSlack.AuthInfo'
+
+            IsAuthenticated = [bool] $Auth.ok
+            Error = if ($Auth.Error) { $Auth.Error } else { }
+
+            Url = $Auth.url
+
+            UserID = $Auth.user_id
+            User = $Auth.user
+
+            TeamID = $Auth.team_id
+            Team = $Auth.team
+
+            Raw = $Auth
+        }
+    }
+}
