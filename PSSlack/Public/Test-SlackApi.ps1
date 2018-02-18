cdd3fbe56124aee28218cb29f85dfcc2a510d751
Sean R. Williams <hmmwhatsthisdo@gmail.com>
2018-02-18 12:46:44 -0800

    Move Test-SlackApi documentation inside of function body

--- PSSlack/Public/Test-SlackApi.ps1
+++ PSSlack/Public/Test-SlackApi.ps1
@@ -1,30 +1,30 @@
-<#
-.SYNOPSIS
-    Tests connectivity to the Slack API.
-.DESCRIPTION
-    This cmdlet calls the Slack API.test method to ensure PSSlack is able to connect to (and invoke) API methods.
-.EXAMPLE
-    # Test connectivity without a token
-    PS C:\> Test-SlackApi
-    
-    # Test connectivity using a specified token
-    PS C:\> Test-SlackApi -Token "xoxp-111-222-333-456789abcdef"
-.INPUTS
-    A Slack API token to use, if desired.
-.OUTPUTS
-    A Boolean value indicating whether the call passed/failed, or the response object from Slack's API.
-.PARAMETER Token
-    The API token to use when communicating with the Slack API.
-.PARAMETER Raw
-    Return the raw response object from the Slack API, versus parsing it and returning a boolean.
-.NOTES
-    Test-SlackApi will not, by default, use the preconfigured Slack API token (as the api.test method does not require authorization).
-.FUNCTIONALITY
-    Slack
-.LINK
-    https://api.slack.com/methods/api.test
-#>
 function Test-SlackApi {
+    <#
+    .SYNOPSIS
+        Tests connectivity to the Slack API.
+    .DESCRIPTION
+        This cmdlet calls the Slack API.test method to ensure PSSlack is able to connect to (and invoke) API methods.
+    .EXAMPLE
+        # Test connectivity without a token
+        PS C:\> Test-SlackApi
+        
+        # Test connectivity using a specified token
+        PS C:\> Test-SlackApi -Token "xoxp-111-222-333-456789abcdef"
+    .INPUTS
+        A Slack API token to use, if desired.
+    .OUTPUTS
+        A Boolean value indicating whether the call passed/failed, or the response object from Slack's API.
+    .PARAMETER Token
+        The API token to use when communicating with the Slack API.
+    .PARAMETER Raw
+        Return the raw response object from the Slack API, versus parsing it and returning a boolean.
+    .NOTES
+        Test-SlackApi will not, by default, use the preconfigured Slack API token (as the api.test method does not require authorization).
+    .FUNCTIONALITY
+        Slack
+    .LINK
+        https://api.slack.com/methods/api.test
+    #>
     [CmdletBinding()]
     param (
         [ValidateNotNullOrEmpty()]
