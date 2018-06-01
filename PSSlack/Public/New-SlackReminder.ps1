e163b024b3ebb0d898b49730a3e2891a7c9bc7e6
Lucas Schill <lukeschill96@gmail.com>
2018-05-31 21:54:29 -0400

    Add commands for managing Slack reminders

---
+++ PSSlack/Public/New-SlackReminder.ps1
@@ -0,0 +1,93 @@
+function New-SlackReminder
+{
+    <#
+    .SYNOPSIS
+        Creates a Slack reminder
+
+    .DESCRIPTION
+        Creates a Slack reminder
+
+    .PARAMETER Token
+        Token to use for the Slack API
+
+        Default value is the value set by Set-PSSlackConfig
+
+        This takes precedence over Uri
+
+    .PARAMETER Proxy
+        Proxy server to use
+
+        Default value is the value set by Set-PSSlackConfig
+
+    .PARAMETER Text
+        The content of the reminder
+
+    .PARAMETER Time
+        When this reminder should happen.  Must be passed in a valid datetime format.
+
+    .PARAMETER User
+        The user who will receive the reminder. If no user is specified, 
+        the reminder will go to user who created it.
+
+        Note: This is not the user's name in Slack but rather the unique user identifier.
+
+    .EXAMPLE
+        # This is a simple example on how to create a reminder
+
+        New-SlackReminder -Text "Do some things" -Time (Get-Date).AddMinutes(1)
+
+        # Creates a reminder with the name "Do some things" set for 1 minute from now
+
+    .FUNCTIONALITY
+        Slack
+    #>
+
+    param (
+
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [string]$Token = $Script:PSSlack.Token,
+
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [string]$Proxy = $Script:PSSlack.Proxy,
+
+        [Parameter(Mandatory = $true,
+            Position = 0)]
+        [String]$Text,
+
+        [Parameter(Mandatory = $true,
+            Position = 1)]
+        [validatescript( {$_ -gt (Get-Date) -and $_ -le (Get-Date).AddYears(5)})]
+        [datetime]$Time,
+
+        [string]$User
+
+    )
+    begin
+    {
+        $ProxyParam = @{}
+        if ($Proxy)
+        {
+            $ProxyParam.Proxy = $Proxy
+        }
+    }
+    process
+    {
+        $body = @{ }
+
+        switch ($psboundparameters.keys)
+        {
+            'text' {$body.text = $text}
+            'time' {$body.time = [Math]::Floor([decimal](Get-Date($time).ToUniversalTime()-uformat "%s"))}
+            'user' {$body.user = $User}
+        }
+    }
+    end
+    {
+        Write-Verbose "Send-SlackApi -Body $body"
+        $response = Send-SlackApi @ProxyParam -Method reminders.add -Body $body -Token $Token -ForceVerbose:$ForceVerbose
+
+        Add-ObjectDetail -InputObject $response.reminder -TypeName 'PSSlack.Reminder'
+    }
+}
---
+++ PSSlack/Public/Set-SlackReminderComplete.ps1
@@ -0,0 +1,115 @@
+function Set-SlackReminderComplete
+{
+    <#
+    .SYNOPSIS
+        Sets the status of a Slack reminder to complete
+
+    .DESCRIPTION
+        Sets the status of a Slack reminder to complete based on the reminder object or reminder id passed to the function
+
+    .PARAMETER Token
+        Token to use for the Slack API
+
+        Default value is the value set by Set-PSSlackConfig
+
+        This takes precedence over Uri
+
+    .PARAMETER Proxy
+        Proxy server to use
+
+        Default value is the value set by Set-PSSlackConfig
+
+    .PARAMETER ReminderId
+        The ID of the reminder that will be deleted.
+
+    .PARAMETER ReminderObject
+        One or more objects of type PSSlack.Reminder. This is intended for use in pipelined scenarios.
+
+    .EXAMPLE
+        # This is a simple example on how to set the status to complete on a Slack reminder
+
+        Set-SlackReminderComplete -ReminderId "RmAZPB1FBP"
+
+        # Sets a Slack reminder with an ID of "RmAZPB1FBP" to complete status
+
+    .EXAMPLE
+        # This is a simple example on how to set the status to complete on a reminder by searching 
+        # and filtering the existing reminders
+        # The returned reminder object is then passed to Set-SlackReminder
+
+        New-SlackReminder -Text "you should complete me soon" -Time (get-date).AddMinutes(10)
+
+        Get-SlackReminder | 
+            Where-Object {$_.Text -like "*complete me*"} |
+            Set-SlackReminderComplete
+
+        # We create a reminder
+        # We then search for that reminder and set the status on it to complete by piping it into Set-SlackReminder
+
+    .EXAMPLE
+        # This is a simple example on how to set the status to complete on multiple reminders 
+        # by searching and filtering the existing reminders
+        # The returned reminder object array is then passed to Set-SlackReminder
+
+        New-SlackReminder -Text "you should complete me soon" -Time (get-date).AddMinutes(10)
+        New-SlackReminder -Text "you should complete me soon too" -Time (get-date).AddMinutes(10)
+
+        Get-SlackReminder | 
+            Where-Object {$_.Text -like "*complete me*"} |
+            Set-SlackReminderComplete
+
+        # We create two reminders
+        # We then search for the reminders and set the status on them to complete by piping them into Set-SlackReminder
+
+    .FUNCTIONALITY
+        Slack
+    #>
+
+    [CmdletBinding(DefaultParameterSetName = 'ByParameter')]
+    param (
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [string]$Token = $Script:PSSlack.Token,
+
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [string]$Proxy = $Script:PSSlack.Proxy,
+
+        [Parameter(ValueFromPipeline = $True)]
+        [Parameter(ParameterSetName = 'ByObject')]
+        [PSTypeName('PSSlack.Reminder')]
+        $ReminderObject,
+
+        [Parameter(ParameterSetName = 'ByParameter')]
+        [Alias("Reminder")]
+        [string]$ReminderId
+    )
+    begin
+    {
+        $ProxyParam = @{}
+        if ($Proxy)
+        {
+            $ProxyParam.Proxy = $Proxy
+        }
+    }
+    process
+    {
+        if ($ReminderObject)
+        {
+            foreach ($object in $ReminderObject)
+            {
+                $body = @{ }
+                $body.reminder = $object.ID
+                Write-Verbose "Send-SlackApi"
+                Send-SlackApi @ProxyParam -Method reminders.complete -Body $body -Token $Token -ForceVerbose:$ForceVerbose
+            }
+        }
+        else
+        {
+            $body = @{ }
+            $body.reminder = $ReminderId
+            Write-Verbose "Send-SlackApi"
+            Send-SlackApi @ProxyParam -Method reminders.complete -Body $body -Token $Token -ForceVerbose:$ForceVerbose
+        }
+    }
+}
