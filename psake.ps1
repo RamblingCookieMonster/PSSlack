1b49ea5cb7f57c91a68361836df3bbdcc19cfe66
Warren Frame <RamblingCookieMonster@users.noreply.github.com>
2018-09-21 06:52:17 -0400

    Switch build to Get-NextNugetPackageVersion [skip ci]

--- psake.ps1
+++ psake.ps1
@@ -65,7 +65,7 @@ Task Build -Depends Test {
     # Bump the module version if we didn't manually bump it
     Try
     {
-        $GalleryVersion = Get-NextPSGalleryVersion -Name $env:BHProjectName -ErrorAction Stop
+        $GalleryVersion = Get-NextNugetPackageVersion -Name $env:BHProjectName -ErrorAction Stop
         $GithubVersion = Get-MetaData -Path $env:BHPSModuleManifest -PropertyName ModuleVersion -ErrorAction Stop
         if($GalleryVersion -ge $GithubVersion) {
             Update-Metadata -Path $env:BHPSModuleManifest -PropertyName ModuleVersion -Value $GalleryVersion -ErrorAction stop
